ef4149d4ee6685b3bd7c814a75acfa8e22bae2c5
Andhika Mahendra <andhikamhndra@gmail.com>
2020-09-06 21:39:16 +0700

    Initial commit

---
+++ constants/products.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    title: 'Mie Instant Rasa Mie Goreng',
+    image: 'https://lemonilo.imgix.net/product/dbecf003e6b308c2eddac24847e661cc.png?w=300&auto=format&q=50',
+    price: 6.750,
+    horizontal: true,
+  },
+  {
+    title: 'Mie Instan Kuah Rasa Kari Ayam',
+    image: 'https://lemonilo.imgix.net/product/21259dd1be0b2c48a2546c87e103e472.png?w=300&auto=format&q=50',
+    price: 220,
+  },
+  {
+    title: 'Mie Instan Kuah Rasa Ayam Bawang',
+    image: 'https://lemonilo.imgix.net/product/fb592766e53cf6f204ebbef812262961.png?w=300&auto=format&q=50',
+    price: 40,
+  },
+  {
+    title: 'Hand Sanitizer Naturizer by Lemonilo Lavender Spray 30 ml',
+    image: 'https://lemonilo.imgix.net/product/e0f9c982c49cbf64ed7eac7d674bd624.png?w=300&auto=format&q=50',
+    price: 188,
+    horizontal: true,
+  },
+  {
+    title: 'Brownies Crispy Rasa Chocochips 40 gr',
+    image: 'https://lemonilo.imgix.net/product/d9b74b13727e2eca290142703a1a0192.png?w=300&auto=format&q=50',
+    price: 180,
+  },
+  {
+    title: 'Brownies Crispy Rasa Keju 40 gr',
+    image: 'https://lemonilo.imgix.net/product/eb74c666338b810da2fbcfcdb01748f9.png?w=300&auto=format&q=50',
+    price: 180,
+  },
+  {
+    title: 'Keripik Ubi Chimi Rasa Jagung Bakar 40 gr',
+    image: 'https://lemonilo.imgix.net/product/39c89c3da5cc27b7edf2753223fe36d4.png?w=300&auto=format&q=50',
+    price: 180,
+  },
+  {
+    title: 'Abon Cabe Chilita 40 Derajat',
+    image: 'https://lemonilo.imgix.net/product/05ab84c8a79049e8a0e335c5edbc075a.png?w=300&auto=format&q=50',
+    price: 180,
+  },
+];
---
+++ navigation/Screens.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Easing, Animated, Dimensions } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import HomeScreen from '../screens/Home';
+
+import CustomDrawerContent from './Menu';
+import { Icon, Header } from '../components';
+import { Images, materialTheme } from "../constants/";
+
+const { width } = Dimensions.get("screen");
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const profile = {
+  avatar: Images.Profile,
+  name: "User"
+};
+
+function HomeStack(props) {
+  return (
+    <Stack.Navigator mode="card" headerMode="screen">
+      <Stack.Screen 
+        name="Home"
+        component={HomeScreen}
+        options={{
+          header: ({ navigation, scene }) => (
+            <Header 
+              search
+              tabs
+              title="Home"
+              navigation={navigation}
+              scene={scene}
+            />
+          )
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function AppStack(props) {
+  return (
+    <Drawer.Navigator
+      style={{ flex: 1 }}
+      drawerContent={props => (
+        <CustomDrawerContent {...props} profile={profile} />
+      )}
+      drawerStyle={{
+        backgroundColor: "white",
+        width: width * 0.8
+      }}
+      drawerContentOptions={{
+        activeTintColor: "white",
+        inactiveTintColor: "#000",
+        activeBackgroundColor: materialTheme.COLORS.ACTIVE,
+        inactiveBackgroundColor: "transparent",
+        itemStyle: {
+          width: width * 0.74,
+          paddingHorizontal: 12,
+          // paddingVertical: 4,
+          justifyContent: "center",
+          alignContent: "center",
+          // alignItems: 'center',
+          overflow: "hidden"
+        },
+        labelStyle: {
+          fontSize: 18,
+          fontWeight: "normal"
+        }
+      }}
+      initialRouteName="Home"
+    >
+      <Drawer.Screen
+        name="Home"
+        component={HomeStack}
+        options={{
+          drawerIcon: ({ focused }) => (
+            <Icon
+              size={16}
+              name="shop"
+              family="GalioExtra"
+              color={focused ? "white" : materialTheme.COLORS.MUTED}
+            />
+          )
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+export default function OnboardingStack(props) {
+  return (
+    <Stack.Navigator mode="card" headerMode="none">
+      <Stack.Screen
+        name="Home"
+        component={AppStack}
+        option={{
+          headerTransparent: true
+        }}
+      />
+      <Stack.Screen name="App" component={AppStack} />
+    </Stack.Navigator>
+  );
+}
